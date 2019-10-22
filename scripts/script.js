13ce664060ca4b4a3a4c8d41354232a24d4fd1ff
Remano <remanovictor@icloud.coms>
2019-10-22 09:15:19 +0200

    added new style sheets

--- scripts/script.js
+++ scripts/script.js
@@ -33,7 +33,27 @@ function start() {
                 let vidSave = doocument.getElementById("vid2");
                 let mediaRecorder = new mediaRecorder(stream);
                 let chunks = [];
+
+                start.addEventListener('click',(ev)=>{
+                    mediaRecorder.start();
+                    console.log(mediaRecorder.state);
+                })
                 
+                stop.addEventListener('click',(ev)=>{
+                    mediaRecorder.stop();
+                    console.log(mediaRecorder.state);
+                });
+
+                mediaRecorder.ondataavailable = function(ev){
+                    chunks.push(ev.data);
+                }
+
+                mediaRecorder.onstop = (ev)=>{
+                    let blob = new Blob(chunks,{'type':'video/mp4;'})
+                    chunks=[];
+                    let videoURL = window.URL.createObjectURL(blob);
+                    vidSave.src = videoURL;
+                }
 
             })
 
